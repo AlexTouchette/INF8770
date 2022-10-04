57a5e9c2d2b765915fc1e4053be065e24fae14b8
MamokaM <codyoko@gmail.com>
2022-10-03 22:03:22 -0400

    added comments to codage predictif

--- Codage_Predictif/Images/image.py
+++ Codage_Predictif/Images/image.py
@@ -1,3 +1,6 @@
+# Algorithm prédictif provenant du cours INF8770 Technologie Multimédia
+# https://github.com/gabilodeau/INF8770/blob/master/Codage%20predictif%20sur%20image.ipynb
+
 import numpy as np
 import matplotlib.pyplot as py
 
@@ -10,24 +13,14 @@ image=imagelue.astype('float')
 image=rgb2gray(image)
 py.imsave("Codage_Predictif\Images\pluton_gray.jpg",image,cmap = py.get_cmap('gray'))
 
-
-''' col=image[:,0]
-image = np.column_stack((col,image))
-col=image[:,len(image[0])-1]
-image = np.column_stack((col,image))
-row=image[0,:]
-image = np.row_stack((row,image))
-row=image[len(image)-1,:]
-image = np.row_stack((row,image))
- '''
 matpred = [[0.33,0.33],[0.33,0.0]]
 
-#erreur = np.zeros((len(image)-2,len(image[0])-2))
+erreur = np.zeros((len(image)-2,len(image[0])-2))
 imagepred = np.zeros((len(image)-2,len(image[0])-2))
 for i in range(1,len(image)-2):
     for j in range(1,len(image[0])-2):
         imagepred[i][j]=image[i-1][j-1]*matpred[0][0]+image[i-1][j]*matpred[0][1]+image[i][j-1]*matpred[1][0]
-        #erreur[i][j]=imagepred[i][j]-image[i][j]
+        erreur[i][j]=imagepred[i][j]-image[i][j]
 
 py.imsave("Codage_Predictif\Images\pluton_gray_compressed.jpg",imagepred,cmap = py.get_cmap('gray'))
       
--- Codage_Predictif/Textes/texte.py
+++ Codage_Predictif/Textes/texte.py
@@ -1,59 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as py
 
-
+# function to decode message 
 def decode(compressed_text,num_list):
+    
     decode=[]
 
     #delete first value of compressed_text since it's always the same
     compressed_text.pop(0)
+
     decode.append(num_list[0])
     for i in range(len(compressed_text)) :
         decode.append(compressed_text[i]+num_list[i])
+    
     return decode
+
+
+
+# path to deferent txt files
 
 #file_name="Codage_Predictif/textes/ABC.txt"
 #file_name="Codage_Predictif/textes/randomText.txt"
-file_name="Codage_Predictif/textes/BeeMovieScript.txt"
+fileName="Codage_Predictif/textes/BeeMovieScript.txt"
+compressedText=[]
 
-file = open(file_name)
+
+# open file  and store it's different lines in a list
+file = open(fileName)
 lines = file.readlines()
-print("LEN",len(lines))
-num_list=[]
-compressed_text=[]
-#read text file and change every character to number
+
+
+# read text file and change every character to number
 for line in lines:
-    num_list = [ord(char) for char in line]
+    numList = [ord(char) for char in line]
 
-fp= open(r'Codage_Predictif/textes/textButNum.txt', 'w')
-for num in num_list :
-    fp.write(str(num))
-#put first number of num_list on compressed_text list since it is unchanged 
-compressed_text.append(num_list[0])
 
-#iterate through num and append compressed_text list with prediction error e = xn - xn-1
-for i in range(1, len(num_list)):
-    error = num_list[i]-num_list[i-1]
-    compressed_text.append(error)
-
-print(num_list)
-print(compressed_text) 
-
-#write new message in file 
-fp= open(r'Codage_Predictif/textes/coded.txt', 'w')
-for num in compressed_text :
+# create a file with containing the original text but converted to number
+# this will help us compare file sizes before and after compression 
+fp= open(r'Codage_Predictif/textes/TextNum.txt', 'w')
+for num in numList :
     fp.write(str(num))
 
 
-#decompresion to find original text
-originalText = decode(compressed_text,num_list)
-''' for num in originalText:
-    print(chr(num)) '''
+# put first number of num_list on compressed_text list since it is unchanged 
+compressedText.append(numList[0])
+
+# iterate through num and append compressed_text list with prediction error e = Xn - X̂n 
+# here X̂n = xn−1
+for i in range(1, len(numList)):
+    error = numList[i]-numList[i-1]
+    compressedText.append(error)
+
+print("ORIGINAL TEXT (numbers)",numList)
+print("ORIGINAL TEXT AFTER COMPRESSION",compressedText)
+
+# write compressed message in file
+# this will help us compare file sizes before and after compression 
+fp= open(r'Codage_Predictif/textes/compressedText.txt', 'w')
+for num in compressedText :
+    fp.write(str(num))
 
 
+# decode compressed message to find original text
+originalText = decode(compressedText,numList)
+print("ORIGINAL TEXT AFTER COMPRESSION",originalText)
 
-
-#print(chr(65))
 
 
 
