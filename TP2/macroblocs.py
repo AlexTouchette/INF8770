ef2f8fa1d9fb234156d08c38c8838206b658f3e4
Alexandre Touchette <alexandre.touchette@polymtl.ca>
2022-11-07 21:46:39 -0500

    Ajout des frames pour les deux seq

--- TP2/macroblocs.py
+++ TP2/macroblocs.py
@@ -1,29 +1,32 @@
 # Première étape: Division de la trame en macroblocs --------------------
 import numpy as np
+import cv2 as cv
 
-# On génère une image aléatoire pour l'instant, on va utiliser les trames
-# du vidéo quand on aura le vidéo
+vidcap = cv.VideoCapture('bouncing_dvd_logo_1.mp4')
 
-grey_levels = 256
-test_image = np.random.randint(0,grey_levels, size=(32, 32))
+
+def get_frames():
+    success,image = vidcap.read()
+    count = 0
+    while success:
+        cv.imwrite("frame%d.jpg" % count, image) # save frame as JPEG file      
+        success,image = vidcap.read()
+        count += 1
+
+
+get_frames()
 
 # On définit la largeur et la hauteur des macroblocs
 largeur = 16
 hauteur = 16
 
-windows = []
-
-
 # On découpe l'image en macroblocs
+"""
 for l in range(0,test_image.shape[0], largeur):
     for h in range(0,test_image.shape[1], hauteur):
         window = test_image[l:l+largeur,h:h+hauteur]
         hist = np.histogram(window,bins=grey_levels)
         windows.append(window)
-        
+"""
 
-# Deuxième étape: Estimation des vecteurs de mouvement ------------------
-
-
-# Troisième étapes: Calcul des blocs de différences ---------------------
 
