2a2441bf5c77d6c9854e2afb5e2ae7a285dbb6ce
Alexandre Touchette <alexandre.touchette@polymtl.ca>
2022-11-07 23:49:59 -0500

    progress i guess

--- TP2/macroblocs.py
+++ TP2/macroblocs.py
@@ -3,6 +3,23 @@ import numpy as np
 import cv2 as cv
 
 vidcap = cv.VideoCapture('bouncing_dvd_logo_1.mp4')
+greyscale = 256
+
+
+class Macroblock:
+    #Doit ajouter vx, vy
+    def __init__(self, x, y, type, CBP, b0, b1, b2, b3):
+        self.x = x
+        self.y = y
+        self.type = type
+        #self.vx = vx
+        #self.vy = vy
+        self.CBP = CBP
+        self.b0 = b0
+        self.b1 = b1
+        self.b2 = b2
+        self.b3 = b3
+    
 
 
 def get_frames():
@@ -13,20 +30,50 @@ def get_frames():
         success,image = vidcap.read()
         count += 1
 
+def get_macroblocks(image, index):
+    # On définit les variation des macroblocs
+    deltaPlein = 16
+    deltaMoitier = 8
 
-get_frames()
+    macroblocks = []
 
-# On définit la largeur et la hauteur des macroblocs
-largeur = 16
-hauteur = 16
+    # On itère au travers de la trame pour isoler chaque macro bloc
+    for x in range(0,image.shape[0], deltaPlein):
+        for y in range(0,image.shape[1], deltaPlein):
+            # Définition du macroblock de départ avec son adresse
+            macroAddrX = x
+            macroAddrY = y
+            
+            # Définition du type du macroblock:
+            macroType = 'I' if index == 0 else 'P'
 
-# On découpe l'image en macroblocs
-"""
-for l in range(0,test_image.shape[0], largeur):
-    for h in range(0,test_image.shape[1], hauteur):
-        window = test_image[l:l+largeur,h:h+hauteur]
-        hist = np.histogram(window,bins=grey_levels)
-        windows.append(window)
-"""
+
+            # Définition du vx et vy:
+            # Still need to figure that out lol
+
+
+            # Définition du CPB (4:0:0):
+            #J'ai mit 4 pour l'instant mais je suis vraiment pas certain
+            imgYCC = 4
+
+            # Définition des 4 blocs Y:
+            b0 = image[x:x+deltaMoitier, y:y+deltaMoitier]                          # Coin supérieur gauche à milieu
+            b1 = image[x+deltaMoitier:x+deltaPlein, y:y+deltaMoitier]               # Milieu haut à milieu droit
+            b2 = image[x:x+deltaMoitier, y+deltaMoitier:y+deltaPlein]               # Milieu gauche à milieu bas
+            b3 = image[x+deltaMoitier:x+deltaPlein, y+deltaMoitier:y+deltaPlein]    # Milieu à coin inférieur droit
+
+            macroblock = Macroblock(macroAddrX, macroAddrY, macroType, imgYCC, b0, b1, b2, b3)
+            macroblocks.append(macroblock)
+    return macroblocks
+
+
+
+macroblocks = []
+
+for i in range(15):
+    frame = cv.imread("FrameSeq1/frame%d.jpg" % i)
+    macroblocks.append(get_macroblocks(frame, i))
+    #print(macroblocks[i][0])
+
 
 
