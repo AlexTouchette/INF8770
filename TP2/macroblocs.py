48e043120963180dd3e04f29b06dba3b968fca23
MamokaM <codyoko@gmail.com>
2022-11-07 23:05:31 -0500

    debugged get_frames function

--- TP2/macroblocs.py
+++ TP2/macroblocs.py
@@ -2,19 +2,36 @@
 import numpy as np
 import cv2 as cv
 
-vidcap = cv.VideoCapture('bouncing_dvd_logo_1.mp4')
+def get_frames(start_time, end_time):
+    success=True
+    # read video 
+    vidcap = cv.VideoCapture('TP2/bouncing_dvd_logo_1.mp4') 
 
+    # get number of frames per sec ()
+    fps = vidcap.get(cv.CAP_PROP_FPS)
 
-def get_frames():
-    success,image = vidcap.read()
-    count = 0
-    while success:
-        cv.imwrite("frame%d.jpg" % count, image) # save frame as JPEG file      
+    # calculate start frame id and end frame id 
+    start_frame_id = int(fps*start_time)
+    end_frame_id = int(fps*end_time)
+
+    count = start_frame_id
+  
+    while success and count<=end_frame_id:
+
+        # set reading position to the wanted frame 
+        vidcap.set(cv.CAP_PROP_POS_FRAMES, count)
         success,image = vidcap.read()
+
+        # save frame as JPEG file
+        cv.imwrite("frame%d.jpg" % count, image) 
+
         count += 1
 
+# get frames from 0.0 to 0.5 sec
+get_frames(0.0,0.5)
 
-get_frames()
+# get frames from 3.0 to 3.5 sec
+get_frames(3.0,3.5)
 
 # On définit la largeur et la hauteur des macroblocs
 largeur = 16
@@ -28,5 +45,3 @@ for l in range(0,test_image.shape[0], largeur):
         hist = np.histogram(window,bins=grey_levels)
         windows.append(window)
 """
-
-
