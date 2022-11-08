260d1a2fb16455d04697d82f29d3e169d8abfdf1
Alexandre Touchette <alexandre.touchette@polymtl.ca>
2022-11-07 21:14:51 -0500

    Ajout du video

--- TP2/macroblocs.py
+++ TP2/macroblocs.py
@@ -1,8 +1,29 @@
-# Première étape: Division de la trame en macroblocs
+# Première étape: Division de la trame en macroblocs --------------------
+import numpy as np
+
+# On génère une image aléatoire pour l'instant, on va utiliser les trames
+# du vidéo quand on aura le vidéo
+
+grey_levels = 256
+test_image = np.random.randint(0,grey_levels, size=(32, 32))
+
+# On définit la largeur et la hauteur des macroblocs
+largeur = 16
+hauteur = 16
+
+windows = []
 
 
-# Deuxième étape: Estimation des vecteurs de mouvement
+# On découpe l'image en macroblocs
+for l in range(0,test_image.shape[0], largeur):
+    for h in range(0,test_image.shape[1], hauteur):
+        window = test_image[l:l+largeur,h:h+hauteur]
+        hist = np.histogram(window,bins=grey_levels)
+        windows.append(window)
+        
+
+# Deuxième étape: Estimation des vecteurs de mouvement ------------------
 
 
-# Troisième étapes: Calcul des blocs de différences
+# Troisième étapes: Calcul des blocs de différences ---------------------
 
